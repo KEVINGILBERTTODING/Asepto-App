697b745f42ec75fa259a95a067ed72431f11214d
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-06-20 00:45:29 +0700

    added karyawan list project

---
+++ app/src/main/java/com/example/asepto/ui/main/karyawan/adapter/project/ProjectAdapter.java
@@ -0,0 +1,97 @@
+package com.example.asepto.ui.main.karyawan.adapter.project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.asepto.R;
+import com.example.asepto.data.model.ProjectModel;
+
+import java.util.List;
+
+public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ViewHolder> {
+    Context context;
+    List<ProjectModel> projectModelList;
+
+    public ProjectAdapter(Context context, List<ProjectModel> projectModelList) {
+        this.context = context;
+        this.projectModelList = projectModelList;
+    }
+
+    @NonNull
+    @Override
+    public ProjectAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_project, parent, false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProjectAdapter.ViewHolder holder, int position) {
+        holder.tvnNamaPerusahaan.setText(projectModelList.get(holder.getAdapterPosition()).getNamaPerusahaan());
+        holder.tvEmail.setText(projectModelList.get(holder.getAdapterPosition()).getEmailPerusahaan());
+        holder.tvProjectScope.setText(projectModelList.get(holder.getAdapterPosition()).getNamaProject());
+        holder.tvTgllMulai.setText(projectModelList.get(holder.getAdapterPosition()).getTglMulai());
+        holder.tvTglSelesai.setText(projectModelList.get(holder.getAdapterPosition()).getTglSelesai());
+        holder.tvDeskripsi.setText(projectModelList.get(holder.getAdapterPosition()).getDeskripsiProject());
+
+        holder.ivAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.lrDetail.setVisibility(View.VISIBLE);
+                holder.ivAction.setVisibility(View.GONE);
+                holder.ivAction2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        holder.ivAction2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                holder.lrDetail.setVisibility(View.GONE);
+                holder.ivAction.setVisibility(View.VISIBLE);
+                holder.ivAction2.setVisibility(View.GONE);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return projectModelList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivAction, ivAction2;
+        TextView tvnNamaPerusahaan, tvEmail, tvProjectScope, tvTgllMulai,
+        tvTglSelesai, tvDeskripsi;
+        LinearLayout lrDetail;
+        Button btnProgress;
+        private RelativeLayout rlAction;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivAction = itemView.findViewById(R.id.ivIconAction);
+            ivAction2 = itemView.findViewById(R.id.ivIconAction2);
+            tvnNamaPerusahaan = itemView.findViewById(R.id.tvNamaPerusahaan);
+            tvEmail = itemView.findViewById(R.id.tvEmailPerusahaan);
+            tvProjectScope = itemView.findViewById(R.id.tvProjectScope);
+            tvTgllMulai = itemView.findViewById(R.id.tvTglMulai);
+            tvTglSelesai = itemView.findViewById(R.id.tvTglSelesai);
+            tvDeskripsi = itemView.findViewById(R.id.tvDeskripsi);
+            lrDetail = itemView.findViewById(R.id.layoutDetail);
+            btnProgress = itemView.findViewById(R.id.btnProgress);
+            rlAction = itemView.findViewById(R.id.rlAction);
+        }
+    }
+}
--- app/src/main/java/com/example/asepto/ui/main/karyawan/project/KaryawanProjectFragment.java
+++ app/src/main/java/com/example/asepto/ui/main/karyawan/project/KaryawanProjectFragment.java
@@ -1,4 +1,4 @@
-package com.example.asepto.ui.main.karyawan.home;
+package com.example.asepto.ui.main.karyawan.project;
 
 import android.app.AlertDialog;
 import android.content.Context;
@@ -8,19 +8,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import com.example.asepto.R;
 import com.example.asepto.data.api.ApiConfig;
 import com.example.asepto.data.api.KaryawanService;
 import com.example.asepto.data.model.ProjectModel;
 import com.example.asepto.databinding.FragmentKaryawanHomeBinding;
-import com.example.asepto.ui.main.auth.AdminLoginActivity;
-import com.example.asepto.ui.main.karyawan.project.KaryawanProjectFragment;
+import com.example.asepto.databinding.FragmentKaryawanProjectBinding;
+import com.example.asepto.ui.main.karyawan.adapter.project.ProjectAdapter;
 import com.example.asepto.util.Constans;
 
 import java.util.List;
@@ -30,11 +32,15 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class KaryawanHomeFragment extends Fragment {
-    private FragmentKaryawanHomeBinding binding;
+public class KaryawanProjectFragment extends Fragment {
+
+    private FragmentKaryawanProjectBinding binding;
+    private List<ProjectModel> projectModelList;
+    private LinearLayoutManager linearLayoutManager;
     private KaryawanService karyawanService;
     private SharedPreferences sharedPreferences;
-    private String userId;
+    private String userId, status;
+    private ProjectAdapter projectAdapter;
     private AlertDialog progressDialog;
 
 
@@ -42,10 +48,12 @@ public class KaryawanHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentKaryawanHomeBinding.inflate(inflater, container, false);
+        binding = FragmentKaryawanProjectBinding.inflate(inflater, container, false);
         sharedPreferences = getContext().getSharedPreferences(Constans.SHARED_PREF_NAME, Context.MODE_PRIVATE);
-        userId = sharedPreferences.getString(Constans.USER_ID, null);
         karyawanService = ApiConfig.getClient().create(KaryawanService.class);
+        userId = sharedPreferences.getString(Constans.USER_ID, null);
+        status = getArguments().getString("status_id");
+        Log.d("cek status", "onCreateView: " + status);
 
         return binding.getRoot();
     }
@@ -53,58 +61,49 @@ public class KaryawanHomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getTotalProject("all", binding.tvTotalProject);
-        getTotalProject("1", binding.tvTotalProjectSelesai);
-        getTotalProject("0", binding.tvTotalProjectSedangBerjalan);
+        getProject(status);
         listener();
     }
 
     private void listener() {
-        binding.cvProjectSelesai.setOnClickListener(new View.OnClickListener() {
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replace("1");
-            }
-        });
-
-        binding.cvTotalProject.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                replace("all");
-            }
-        });
-        binding.cvTotalProjectSedangBerjalan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                replace("0");
+                getActivity().onBackPressed();
             }
         });
     }
 
-    private void getTotalProject(String status, TextView tvTotal) {
+    private void getProject(String status) {
         showProgressBar("Loading", "Memuat data...", true);
         karyawanService.getMyProject(userId, status).enqueue(new Callback<List<ProjectModel>>() {
             @Override
             public void onResponse(Call<List<ProjectModel>> call, Response<List<ProjectModel>> response) {
                 showProgressBar("d", "d", false);
                 if (response.isSuccessful() && response.body().size() > 0) {
-                    tvTotal.setText(String.valueOf(response.body().size()));
+                    projectModelList = response.body();
+                    projectAdapter = new ProjectAdapter(getContext(), projectModelList);
+                    linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+                    binding.rvProject.setLayoutManager(linearLayoutManager);
+                    binding.rvProject.setAdapter(projectAdapter);
+                    binding.rvProject.setHasFixedSize(true);
+                    binding.tvEmpty.setVisibility(View.GONE);
+
                 }else {
-                    tvTotal.setText("0");
+                    binding.tvEmpty.setVisibility(View.VISIBLE);
                 }
             }
 
             @Override
             public void onFailure(Call<List<ProjectModel>> call, Throwable t) {
                 showProgressBar("d", "d", false);
+                binding.tvEmpty.setVisibility(View.VISIBLE);
                 showToast("err", "Tidak ada koneksi internet");
-
 
             }
         });
 
     }
-
 
     private void showProgressBar(String title, String message, boolean isLoading) {
         if (isLoading) {
@@ -124,6 +123,7 @@ public class KaryawanHomeFragment extends Fragment {
             }
         }
     }
+
     private void showToast(String jenis, String text) {
         if (jenis.equals("success")) {
             Toasty.success(getContext(), text, Toasty.LENGTH_SHORT).show();
@@ -135,7 +135,7 @@ public class KaryawanHomeFragment extends Fragment {
     private void replace(String status){
         Fragment fr = new KaryawanProjectFragment();
         Bundle bundle = new Bundle();
-        bundle.putString("status_id", status);
+        bundle.getString("status", status);
         fr.setArguments(bundle);
         getActivity().getSupportFragmentManager().beginTransaction()
                 .replace(R.id.frameKaryawn, fr).addToBackStack(null)
